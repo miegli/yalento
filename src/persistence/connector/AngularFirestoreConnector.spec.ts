2badb7dbc31d4762d75c0ec8d2d4051bb4bddf2a
michaelegli <michael.egli@as-software.ch>
2019-10-31 16:04:05 +0100

    add dotenv

--- src/persistence/connector/AngularFirestoreConnector.spec.ts
+++ src/persistence/connector/AngularFirestoreConnector.spec.ts
@@ -7,6 +7,9 @@ import {describe, it} from 'mocha';
 import {take} from "rxjs/operators";
 import {Repository} from '../Repository';
 
+// tslint:disable-next-line:no-var-requires
+require('dotenv').config();
+
 export class Contact {
 
     public name: string = '';
@@ -23,20 +26,29 @@ describe('AngularFirestoreConnectorTest', async () => {
         const name = 'name' + Guid.create();
         const repo = new Repository<Contact>(Contact);
         const fb = firebase.initializeApp({
-            apiKey: "AIzaSyAgHaHlFGw6bVr5WDscLG8Kn8jF_rYJgGY",
-            authDomain: "yalento-test.firebaseapp.com",
-            databaseURL: "https://yalento-test.firebaseio.com",
-            projectId: "yalento-test",
-            storageBucket: "yalento-test.appspot.com",
-            messagingSenderId: "252117874295",
-            appId: "1:252117874295:web:55f6e8a5e8a50096627872",
-            measurementId: "G-DKEPQWT1H3",
+            apiKey: process.env.test_firebase_apiKey,
+            authDomain: process.env.test_firebase_authDomain,
+            databaseURL: process.env.test_firebase_databaseURL,
+            projectId: process.env.test_firebase_projectId,
+            storageBucket: process.env.test_firebase_storageBucket,
+            messagingSenderId: process.env.test_firebase_messagingSenderId,
+            appId: process.env.test_firebase_appId,
+            measurementId: process.env.test_firebase_measurementId,
         }, 'test' + Guid.create());
 
         repo.connectFirestore(fb);
 
-        expect(await repo.create({name: name}, 'test1')).to.be.deep.equal({name: name, lastName: '', street: '', age: 1});
-        expect(await repo.createMany([{name: name, age: 1, '__uuid': 'test2'}, {name: name, age: 2, '__uuid': 'test3'}])).to.be.lengthOf(2);
+        expect(await repo.create({name: name}, 'test1')).to.be.deep.equal({
+            name: name,
+            lastName: '',
+            street: '',
+            age: 1
+        });
+        expect(await repo.createMany([{name: name, age: 1, '__uuid': 'test2'}, {
+            name: name,
+            age: 2,
+            '__uuid': 'test3'
+        }])).to.be.lengthOf(2);
         expect(await repo.select().getResultsAsPromise()).to.be.lengthOf(3);
 
         const repo2 = new Repository<Contact>(Contact);
