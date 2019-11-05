bbd2f1ca3ead18a0076803b63753c6d17b934953
michaelegli <michael.egli@as-software.ch>
2019-11-05 11:50:51 +0100

    add non-realtime mode

--- src/persistence/connector/FirestoreConnector.ts
+++ src/persistence/connector/FirestoreConnector.ts
@@ -1,128 +1,147 @@
 import * as firesql from 'firesql/firesql.umd.js';
 import 'firesql/rx';
-import { throwError } from 'rxjs';
-import { Repository } from '../Repository';
-import { AbstractConnector } from './AbstractConnector';
-import { IConnectorInterface } from './ConnectorInterface';
+import {throwError} from 'rxjs';
+import {Repository} from '../Repository';
+import {AbstractConnector} from './AbstractConnector';
+import {IConnectorInterface} from './ConnectorInterface';
 
-export class Firestore {}
+export class Firestore {
+}
 
 export declare class User {
-  public readonly uid: string | null;
+    public readonly uid: string | null;
 
-  constructor(uid: string | null);
+    constructor(uid: string | null);
 
-  public isAuthenticated(): boolean;
+    public isAuthenticated(): boolean;
 }
 
 export type ConnectionFirestoreDataMode = 'PRIVATE' | 'ALL';
 
 export interface IConnectionFirestore {
-  path?: string;
-  sql?: string;
-  dataMode?: ConnectionFirestoreDataMode;
+    path?: string;
+    sql?: string;
+    dataMode?: ConnectionFirestoreDataMode;
+    realtimeMode?: boolean;
 }
 
 export class FirestoreConnector<T> extends AbstractConnector<T> implements IConnectorInterface<T> {
-  private readonly db: Firestore;
-  private readonly currentUser: User;
-  private firesSQL: any;
-  private lastSql: string = '';
-  private rxQuerySubscriber: any;
-  private dataMode: ConnectionFirestoreDataMode = 'ALL';
+    private readonly db: Firestore;
+    private readonly currentUser: User;
+    private readonly dataMode: ConnectionFirestoreDataMode = 'ALL';
+    private readonly realtimeMode: boolean = true;
+    private firesSQL: any;
+    private lastSql: string = '';
+    private rxQuerySubscriber: any;
 
-  constructor(repository: Repository<T>, db: Firestore, options?: IConnectionFirestore) {
-    super(repository, options);
+    constructor(repository: Repository<T>, db: Firestore, options?: IConnectionFirestore) {
+        super(repository, options);
 
-    if (options && options.dataMode) {
-      this.dataMode = options.dataMode;
+        if (options && options.dataMode) {
+            this.dataMode = options.dataMode;
+        }
+
+        if (options && options.realtimeMode === false) {
+            this.realtimeMode = false;
+        }
+
+        /* istanbul ignore next */
+        if (db.constructor.name !== 'AngularFirestore') {
+            this.db = (db as any).firestore();
+        } else {
+            this.db = (db as any).firestore;
+        }
+
+        this.currentUser = (this.db as any)._credentials.currentUser;
+
+        this.firesSQL = new firesql.FireSQL(this.db);
     }
 
-    /* istanbul ignore next */
-    if (db.constructor.name !== 'AngularFirestore') {
-      this.db = (db as any).firestore();
-    } else {
-      this.db = (db as any).firestore;
-    }
+    public add(items: T[]) {
+        items.forEach((item: any) => {
+            const data = item._toPlain();
+            data['__uuid'] = item['_uuid'];
+            data['__owner'] = {};
 
-    this.currentUser = (this.db as any)._credentials.currentUser;
+            if (this.currentUser.uid && this.currentUser.uid !== 'null') {
+                data['__owner'][this.currentUser.uid] = true;
+            }
 
-    this.firesSQL = new firesql.FireSQL(this.db);
-  }
+            if (this.dataMode !== 'PRIVATE') {
+                data['__owner']['EVERYBODY'] = true;
+            }
 
-  public add(items: T[]) {
-    items.forEach((item: any) => {
-      const data = item._toPlain();
-      data['__uuid'] = item['_uuid'];
-      data['__owner'] = {};
-
-      if (this.currentUser.uid && this.currentUser.uid !== 'null') {
-        data['__owner'][this.currentUser.uid] = true;
-      }
-
-      if (this.dataMode !== 'PRIVATE') {
-        data['__owner']['EVERYBODY'] = true;
-      }
-
-      (this.db as any)
-        .doc(this.getPath() + '/' + item._uuid)
-        .set(data, { merge: true })
-        .then()
-        .catch(e => {
-          throwError(
-            'error while creating firestore document "' + this.getPath() + '/' + item._uuid + '": ' + e.message,
-          );
+            (this.db as any)
+                .doc(this.getPath() + '/' + item._uuid)
+                .set(data, {merge: true})
+                .then()
+                .catch(e => {
+                    throwError(
+                        'error while creating firestore document "' + this.getPath() + '/' + item._uuid + '": ' + e.message,
+                    );
+                });
         });
-    });
 
-    return;
-  }
-
-  public select(sql: string) {
-    let finalSql = !this.options || !this.options.sql ? sql : this.options.sql;
-    let finalSqlCondition = finalSql.substr(('SELECT * FROM ' + this.repository.getClassName()).length).trim();
-    let finalSqlConditionUser = '';
-
-    if (this.dataMode === 'PRIVATE') {
-      finalSqlConditionUser = '`__owner.' + this.currentUser.uid + '` = true ';
-    } else {
-      finalSqlConditionUser = '`__owner.EVERYBODY` = true ';
-      if (this.currentUser.uid && this.currentUser.uid !== 'null') {
-        finalSqlCondition += ' OR `__owner.' + this.currentUser.uid + '` = true ';
-      }
+        return;
     }
 
-    if (finalSqlCondition.length > 0 && finalSqlCondition.substr(0, 5) === 'WHERE') {
-      finalSqlCondition = ' AND ' + finalSqlCondition.substr(6) + ' ';
+    public select(sql: string) {
+        let finalSql = !this.options || !this.options.sql ? sql : this.options.sql;
+        let finalSqlCondition = finalSql.substr(('SELECT * FROM ' + this.repository.getClassName()).length).trim();
+        let finalSqlConditionUser = '';
+
+        if (this.dataMode === 'PRIVATE') {
+            finalSqlConditionUser = '`__owner.' + this.currentUser.uid + '` = true ';
+        } else {
+            finalSqlConditionUser = '`__owner.EVERYBODY` = true ';
+            if (this.currentUser.uid && this.currentUser.uid !== 'null') {
+                finalSqlCondition += ' OR `__owner.' + this.currentUser.uid + '` = true ';
+            }
+        }
+
+        if (finalSqlCondition.length > 0 && finalSqlCondition.substr(0, 5) === 'WHERE') {
+            finalSqlCondition = ' AND ' + finalSqlCondition.substr(6) + ' ';
+        }
+
+        const where = finalSqlConditionUser.length || finalSqlCondition.length ? ' WHERE ' : '';
+
+        finalSql = 'SELECT * FROM ' + this.getPath() + where + finalSqlConditionUser + finalSqlCondition;
+
+        if (this.lastSql !== finalSql) {
+
+
+            if (this.realtimeMode) {
+                const data$ = this.firesSQL.rxQuery(finalSql);
+
+                if (this.rxQuerySubscriber) {
+                    this.rxQuerySubscriber.unsubscribe();
+                }
+
+                this.rxQuerySubscriber = data$.subscribe((results: any) => {
+                    this.repository
+                        .createMany(results, '', 'firestore')
+                        .then()
+                        .catch();
+                });
+
+            } else {
+                this.firesSQL.query(finalSql).then((results: any) => {
+                    this.repository
+                        .createMany(results, '', 'firestore')
+                        .then()
+                        .catch();
+                });
+            }
+
+            this.lastSql = finalSql;
+        }
+
+        return;
     }
 
-    const where = finalSqlConditionUser.length || finalSqlCondition.length ? ' WHERE ' : '';
-
-    finalSql = 'SELECT * FROM ' + this.getPath() + where + finalSqlConditionUser + finalSqlCondition;
-
-    if (this.lastSql !== finalSql) {
-      const data$ = this.firesSQL.rxQuery(finalSql);
-
-      if (this.rxQuerySubscriber) {
-        this.rxQuerySubscriber.unsubscribe();
-      }
-
-      this.rxQuerySubscriber = data$.subscribe((results: any) => {
-        this.repository
-          .createMany(results, '', 'firestore')
-          .then()
-          .catch();
-      });
-
-      this.lastSql = finalSql;
+    public disconnect(): void {
+        if (this.rxQuerySubscriber) {
+            this.rxQuerySubscriber.unsubscribe();
+        }
     }
-
-    return;
-  }
-
-  public disconnect(): void {
-    if (this.rxQuerySubscriber) {
-      this.rxQuerySubscriber.unsubscribe();
-    }
-  }
 }
