01de58cf8181cbed55116952b3c54f1ef0a0c049
michaelegli <michael.egli@as-software.ch>
2019-12-10 15:55:58 +0100

    add QueryPaginator export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Select } from './persistence/select/select';
 export { Repository, IEntity, IGeoData } from './persistence/Repository';
-export { IPageEventSort } from './persistence/query/QueryPaginator';
+export { IPageEventSort, QueryPaginator } from './persistence/query/QueryPaginator';
 export { IsMatch } from './helper/isMatch';
 export { IConnectionFirestoreNearBy } from './persistence/connector/FirestoreConnector';
