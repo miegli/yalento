9da9ae9e08021bd7d3f81dcef2f9dd979e39a8d7
michaelegli <michael.egli@as-software.ch>
2020-03-15 22:51:25 +0100

    fix test

--- src/persistence/connector/FirestoreConnector.spec.ts
+++ src/persistence/connector/FirestoreConnector.spec.ts
@@ -46,6 +46,7 @@ describe('FirestoreConnectorTest', async () => {
     const name = 'name' + Guid.create();
 
     expect(await repo.create({ name: name }, 'test1')).to.be.deep.equal({
+      __initialdata: "{\"name\":\""+name+"\"}",
       __owner: {
         EVERYBODY: true,
       },
